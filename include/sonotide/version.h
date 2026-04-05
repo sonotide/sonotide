677a163ecf35fa788ecf974e5e60c7e70543c81d
Gamshik <glebovich157rtyp@gmail.com>
2026-04-06 02:01:36 +0300

    chore: bump version to 0.3.0

--- include/sonotide/version.h
+++ include/sonotide/version.h
@@ -7,10 +7,10 @@ namespace sonotide {
 /// Мажорная версия пакета Sonotide.
 inline constexpr int version_major = 0;
 /// Минорная версия пакета Sonotide.
-inline constexpr int version_minor = 2;
+inline constexpr int version_minor = 3;
 /// Патч-версия пакета Sonotide.
 inline constexpr int version_patch = 0;
 /// Человекочитаемая semantic version строка.
-inline constexpr std::string_view version_string = "0.2.0";
+inline constexpr std::string_view version_string = "0.3.0";
 
 }  // namespace sonotide
